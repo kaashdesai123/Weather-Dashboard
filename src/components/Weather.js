d80d95aa4d7fb3e55aac1aa5c3e2f6d6fc130d32
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 21:28:46 -0700

    Create Weather.js

---
+++ src/components/Weather.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const API_KEY = "YOUR_OPENWEATHERMAP_API_KEY";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+function Weather() {
+    const [city, setCity] = useState("");
+    const [weatherData, setWeatherData] = useState(null);
+
+    const fetchWeather = async () => {
+        try {
+            const response = await axios.get(BASE_URL, {
+                params: {
+                    q: city,
+                    units: "metric",
+                    appid: API_KEY
+                }
+            });
+
+            setWeatherData(response.data);
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+        }
+    };
+
+    return (
+        <div>
+            <input 
+                type="text" 
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="Enter city"
+            />
+            <button onClick={fetchWeather}>Get Weather</button>
+
+            {weatherData && (
+                <div>
+                    <h2>{weatherData.name}</h2>
+                    <p>{weatherData.weather[0].description}</p>
+                    <p>Temperature: {weatherData.main.temp}°C</p>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Weather;
