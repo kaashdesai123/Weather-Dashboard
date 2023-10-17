5177a56c0edc01b067a0e3f9ec2435be97f9b92a
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 21:29:07 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './components/Weather';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Weather Dashboard</h1>
+            <Weather />
+        </div>
+    );
+}
+
+export default App;
